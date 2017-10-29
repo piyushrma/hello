39e0a9a5cdcce6a15f70e59e861482b8ffd11f60
Albert09 <33201831+Albert09@users.noreply.github.com>
2017-10-29 22:19:14 +0530

    Create lcm.cpp

---
+++ lcm.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a, b, x, y, t, hcf, lcm;
+	cout<<"Enter two number : ";
+	cin>>x>>y;
+	a=x;
+	b=y;
+	while(b!=0)
+	{
+		t=b;
+		b=a%b;
+		a=t;
+	}
+	hcf=a;
+	lcm=(x*y)/hcf;
+	cout<<"LCM = "<<lcm<<"\n";
+	getch();
+}
